3b1bcdc26992bb99858ce97bbac948b4e4c15300
Sulaiman Abu Ghazal <your-sulaiman@windows.com>
2024-05-21 10:56:42 +0300

    My First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# API-Project
+My First API Project !
---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+//var geoip = import("geoip-lite");
+
+const app = express();
+app.use(bodyParser.json()); // This middleware parses JSON-formatted request bodies.
+const port = 3000;
+  app.use(express.static("public"));
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.get("/", (req, res) => {
+    res.render("home.ejs");
+   // console.log(req);
+  });
+
+  app.post("/location", async (req, res) => {
+     try {
+      //const response = await axios.get("https://bored-api.appbrewery.com/filter?type=" + req.body.type + "&participants=" + req.body.participants);
+      //const response = await axios.get("https://bored-api.appbrewery.com/filter?type=" + req.body.type + "&participants=" + req.body.participants);
+      //const result = response.data;
+       //console.log(result[0]);
+       const { latitude, longitude } = req.body;
+       console.log(`Received latitude: ${latitude}, longitude: ${longitude}`);
+       //in google earth u have to put the latitude first then comma the longitude
+       res.status(200).json({ message: 'Data received successfullylengle' });
+     } catch (error) {
+       console.error("Failed to make request:", error.message);
+       res.render("index.ejs", {
+         error: error.message,
+       });
+     }
+   });
+ 
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
+
